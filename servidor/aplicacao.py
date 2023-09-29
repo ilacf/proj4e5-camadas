deb4b6d24a5c5813c72734b202befee10f96d033
AnandaCampelo <anandag.c@hotmail.com>
2023-09-29 17:53:00 -0300

    not the crc

--- servidor/aplicacao.py
+++ servidor/aplicacao.py
@@ -2,8 +2,28 @@ from enlace import *
 import time
 import base64
 import numpy as np
+from datetime import datetime
+from crc import Crc16, Calculator
 
 serialName = "COM5"
+
+crc = Calculator(Crc16.CCITT)
+
+def arquivo(nome, head, pl, eop, env_receb, tipo_de_erro=''):
+    data_hora = datetime.now()
+    # tipo = int.from_bytes(head[0], byteorder='big')
+    tipo = head[0]
+    tamanho_bytes = len(head + pl + eop)
+    conteudo = f"{data_hora} / {env_receb} / {tipo} / {tamanho_bytes}"
+    if tipo == 3:
+        conteudo += f" / {head[4]} / {head[3]} / crc"
+        if tipo_de_erro != '':
+            conteudo += f" / {tipo_de_erro}"
+    elif tipo == 1 or tipo == 2:
+        conteudo += " / handshake"
+    with open(f"{nome}", "a") as file:
+        file.write(conteudo)
+        file.write("\n")
 
 def main():
     try:
@@ -28,6 +48,8 @@ def main():
         pl, _ = com1.getData(head[5])
         eop_receb, _ = com1.getData(4)
 
+        arquivo('arquivo1.txt', head, pl, eop_receb, 'receb')
+
         while ocioso:
             if head[0] == 1 and head[1] == 1:
                 print("\nRecebeu mensagem tipo 1\n")
@@ -36,6 +58,8 @@ def main():
             time.sleep(1)
 
         numPckg = head[3]
+
+        recebido = ''
 
         h0_t2 = (2).to_bytes(1, byteorder="big")
         h1 = b'\x00'
@@ -58,6 +82,8 @@ def main():
         time.sleep(0.1)
         print("\nMensagem tipo 2 enviada\n")
         print("-------------------------")
+
+        arquivo('arquivo1.txt', head_t2, pl_t2, eop, 'envio')
 
         cont = 0
 
@@ -88,11 +114,20 @@ def main():
                 print(f"\nPacote de número {cont} recebido\n")
                 print("-------------------------")
 
-                if head_receb[0] == 3 and eop_receb == eop:
+                checando_crc = crc.checksum(pl_receb)
+
+                print(f"\n{checando_crc}\n")
+                print(f'\n{(head_receb[8]).to_bytes(2,byteorder="big")}\n')
+
+                if head_receb[0] == 3 and eop_receb == eop and checando_crc == (head_receb[8]):
                     # if payload correto e num pacote esperado correto
                     if head_receb[4] >= 0 and head_receb[4] <= head_receb[3] and head_receb[4] not in lista_packages and head_receb[4] == lista_packages[-1] + 1:
+                        arquivo('arquivo1.txt', head_receb, pl_receb, eop_receb, 'receb')
+                        
                         print(f"\nMensagem tipo 3 recebida corretamente, pacote de número {head_receb[4]}\n")
                         print("-------------------------")
+
+                        recebido += pl_receb
 
                         lista_packages.append(head_receb[4])
                         # aa = recebeu certo, bb = recebeu errado, cc = recebeu ultimo
@@ -117,12 +152,16 @@ def main():
                         com1.sendData(np.asarray(msg_t4))
                         time.sleep(0.1)
 
+                        arquivo('arquivo1.txt', head_t4, pl_t4, eop, 'envio')
+
                         print(f"\nMensagem tipo 4 enviada, pacote de número {head_receb[4]}\n")
 
                         cont += 1
                         break
 
                     elif (head_receb[4] < 0 or head_receb[4] > head_receb[3]):
+                        arquivo('arquivo2.txt', head_receb, pl_receb, eop_receb, 'receb')
+                        
                         print(f"\nMensagem tipo 3 recebida com erro 1\n")
                         print("-------------------------")
                         
@@ -144,11 +183,15 @@ def main():
                         com1.sendData(np.asarray(head_t6 + pl_t6 + eop))
                         time.sleep(0.1)
 
+                        arquivo('arquivo2.txt', head_t6, pl_t6, eop, 'envio')
+
                         print(f"\nMensagem tipo 6 enviada\n")
                         break
 
                     else:
                         if head_receb[4] in lista_packages:
+                            arquivo('arquivo2.txt', head_receb, pl_receb, eop_receb, 'receb')
+
                             print(f"\nMensagem tipo 3 recebida com erro 2\n")
 
                             h0_t6 = (6).to_bytes(1, byteorder="big")
@@ -169,10 +212,14 @@ def main():
                             com1.sendData(np.asarray(head_t6 + pl_t6 + eop))
                             time.sleep(0.1)
 
+                            arquivo('arquivo2.txt', head_t6, pl_t6, eop, 'envio')
+
                             print(f"\nMensagem tipo 6 enviada\n")
                             break
                             
                         elif head_receb[4] != lista_packages[-1] + 1:
+                            arquivo('arquivo2.txt', head_receb, pl_receb, eop_receb, 'receb')
+
                             print(f"\nMensagem tipo 3 recebida com erro 3\n")
 
                             h0_t6 = (6).to_bytes(1, byteorder="big")
@@ -192,6 +239,8 @@ def main():
 
                             com1.sendData(np.asarray(head_t6 + pl_t6 + eop))
                             time.sleep(0.1)
+
+                            arquivo('arquivo2.txt', head_t6, pl_t6, eop, 'envio')
 
                             print(f"\nMensagem tipo 6 enviada\n")
                             break
@@ -220,6 +269,8 @@ def main():
 
                             com1.sendData(np.asarray(head_t4 + pl_t4 + eop))
 
+                            arquivo('arquivo4.txt', head_t4, pl_t4, eop, 'envio')
+
                             timer_1 = time.time()
                     else:
                         ocioso = True
@@ -230,6 +281,8 @@ def main():
 
                         com1.sendData(np.asarray(head_t5 + pl_5 + eop))
 
+                        arquivo('arquivo3.txt', head_t5, pl_5, eop, 'envio')
+
                         print("\nMensagem tipo 5 enviada\n")
                         print("Time Out\n")
 
@@ -238,6 +291,15 @@ def main():
         if cont == numPckg:
             print("\nTodas as mensagens recebidas com sucesso\n")
             print("-------------------------")
+
+            head_termino = b'x00' + numPckg + (1).to_bytes(1, byteorder="big") + b'\xaa'*9
+            com1.sendData(head_termino + b'\xdd' + eop)
+            time.sleep(0.1)
+
+            arquivo('arquivo1.txt', head_termino, b'\xdd', eop, 'envio')
+
+            with open('output.png', 'wb') as image_file:
+                image_file.write(recebido)
 
         print("-------------------------")
         print("Comunicação encerrada")
